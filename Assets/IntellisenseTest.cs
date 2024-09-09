9c7e81f701d02bb9be984cf5d7011bd9cbd4db6c
kris <kkuerten@inside.artcenter.edu>
2024-09-09 09:34:11 -0700

    Intellisense Test

---
+++ Assets/IntellisenseTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntellisenseTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float krisVariable = 1f;
+        krisVariable += 1;
+        krisVariable -= 1;
+    }
+}
